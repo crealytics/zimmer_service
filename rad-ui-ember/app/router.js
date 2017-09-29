62f7ea9f17feccb7cf3eabf64cc9264023c1df4b
Michal Olah <olahmichal@gmail.com>
2017-09-29 13:36:13 +0200

    adding navigation

--- rad-ui-ember/app/router.js
+++ rad-ui-ember/app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('login', { path: '/' });
+  this.route('login', { path: '/login' });
   this.route('bookings', { path: '/bookings' });
 });
 
